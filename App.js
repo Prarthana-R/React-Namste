97b962e5a7e97748c1558c3c99029643785c1ac8
Prarthana <prarthanahyr6@gmail.com>
2025-12-26 14:17:10 +0530

    Episode-2 (Igniting our app)

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-/*******
- * 
- * <div id = "parent">
- *    <div id = "child">
- *      <h1>I am h1 tag </h1>
- *      <h2> i am h2 tag </h2>
- *    </div>
- *    <div id = "child2">
- *      <h1>I am h1 tag </h1>
- *      <h2> i am h2 tag </h2>
- *    </div>
- * </div>
- * 
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {id: "parent"},
     [
         React.createElement("div", {id: "child"}, [
-    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "Prarthana"),
     React.createElement("h2", {}, "I am h2 tag")
 ]
 ),
@@ -28,9 +15,5 @@ React.createElement("div", {id: "child2"}, [
 )
     ] 
 );
-
-
-
-console.log(parent)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namste React (Episode 1)
+
+# Parcel
+
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm (Wriiten in C ++)
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differntial Bundling
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Differnt dev and prod bundles
